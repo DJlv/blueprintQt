9f7c02559b006dd73cb1331a3a96833cc4819922
‘DJlv’ <2743535685@qq.com>
2024-01-06 18:17:23 +0800

    init

--- Sources/Cpp/Port/BP_BasePort.cpp
+++ Sources/Cpp/Port/BP_BasePort.cpp
@@ -8,17 +8,20 @@
 void BP_BasePort::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) {
     QFont font("Consolas", 12);  // 创建字体对象并指定字体名称和大小
     painter->setFont(font);
-    QPen pen_default = QPen(QColor(Color));
+    QPen pen_default = QPen(QColor("#ffffff"));
     pen_default.setWidthF(1.5);
-    QBrush brush_default = QBrush(QColor(Color));
     painter->setPen(pen_default);
-    painter->setBrush(Qt::NoBrush);
+
     if (port_type == PinType::port_type_port_in) {
-        painter->drawText(Title.size() * 2 + 20, 12, Title);
+        painter->drawText(Title.size()+ 10, 12, Title);
     } else if (port_type == PinType::port_type_port_out) {
-        painter->drawText(-Title.size() * 2 - 20 - 15, 12, Title);
+        painter->drawText(-Title.size() * 9, 12, Title);
     }
-    if(!flage) {
+
+    QPen brush_default = QPen(QColor(Color));
+    painter->setPen(brush_default);
+    painter->setBrush(Qt::NoBrush);
+    if (!flage) {
         painter->drawEllipse(0, 0, 15, 15);
     } else {
         QColor color(Color);
@@ -26,28 +29,26 @@ void BP_BasePort::paint(QPainter *painter, const QStyleOptionGraphicsItem *optio
         painter->drawEllipse(0, 0, 15, 15);
         painter->drawEllipse(0, 0, 15, 15);
     }
-    if(edgeList.size() > 0) {
-        flage =true;
+    if (edgeList.size() > 0) {
+        flage = true;
     } else {
-        flage =false;
+        flage = false;
     }
-    QGraphicsSimpleTextItem
     update();
 }
 
-void BP_BasePort::add_Edge(BP_Edge* edgeItem)
-{
+void BP_BasePort::add_Edge(BP_Edge *edgeItem) {
     edgeList.append(edgeItem);
 }
 
-BP_BasePort::BP_BasePort(QGraphicsItem* parent){
+BP_BasePort::BP_BasePort(QGraphicsItem *parent) {
 }
 
 BP_BasePort::BP_BasePort(PinType type, QGraphicsItem *parent) : QGraphicsItem(parent), port_type(type) {
     update();
 }
 
-void BP_BasePort::add_to_parent_node(BP_BaseNode* nodeItem) {
+void BP_BasePort::add_to_parent_node(BP_BaseNode *nodeItem) {
     node = nodeItem;
 }
 
